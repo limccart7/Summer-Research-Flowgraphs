91eab0766020b5a2f22d1af3bfb765b2d5bb21d9
limccart7 <145800942+limccart7@users.noreply.github.com>
2024-07-03 15:43:56 -0700

    Updated to use Soapy

---
+++ LoRaOTALoopback.py
@@ -0,0 +1,188 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+#
+# SPDX-License-Identifier: GPL-3.0
+#
+# GNU Radio Python Flow Graph
+# Title: LoRa OTA Loopback
+# Author: root
+# GNU Radio version: 3.10.4.0
+
+from packaging.version import Version as StrictVersion
+
+if __name__ == '__main__':
+    import ctypes
+    import sys
+    if sys.platform.startswith('linux'):
+        try:
+            x11 = ctypes.cdll.LoadLibrary('libX11.so')
+            x11.XInitThreads()
+        except:
+            print("Warning: failed to XInitThreads()")
+
+from gnuradio import blocks
+import pmt
+from gnuradio import gr
+from gnuradio.filter import firdes
+from gnuradio.fft import window
+import sys
+import signal
+from PyQt5 import Qt
+from argparse import ArgumentParser
+from gnuradio.eng_arg import eng_float, intx
+from gnuradio import eng_notation
+from gnuradio import soapy
+import gnuradio.lora_sdr as lora_sdr
+
+
+
+from gnuradio import qtgui
+
+class LoRaOTALoopback(gr.top_block, Qt.QWidget):
+
+    def __init__(self):
+        gr.top_block.__init__(self, "LoRa OTA Loopback", catch_exceptions=True)
+        Qt.QWidget.__init__(self)
+        self.setWindowTitle("LoRa OTA Loopback")
+        qtgui.util.check_set_qss()
+        try:
+            self.setWindowIcon(Qt.QIcon.fromTheme('gnuradio-grc'))
+        except:
+            pass
+        self.top_scroll_layout = Qt.QVBoxLayout()
+        self.setLayout(self.top_scroll_layout)
+        self.top_scroll = Qt.QScrollArea()
+        self.top_scroll.setFrameStyle(Qt.QFrame.NoFrame)
+        self.top_scroll_layout.addWidget(self.top_scroll)
+        self.top_scroll.setWidgetResizable(True)
+        self.top_widget = Qt.QWidget()
+        self.top_scroll.setWidget(self.top_widget)
+        self.top_layout = Qt.QVBoxLayout(self.top_widget)
+        self.top_grid_layout = Qt.QGridLayout()
+        self.top_layout.addLayout(self.top_grid_layout)
+
+        self.settings = Qt.QSettings("GNU Radio", "LoRaOTALoopback")
+
+        try:
+            if StrictVersion(Qt.qVersion()) < StrictVersion("5.0.0"):
+                self.restoreGeometry(self.settings.value("geometry").toByteArray())
+            else:
+                self.restoreGeometry(self.settings.value("geometry"))
+        except:
+            pass
+
+        ##################################################
+        # Variables
+        ##################################################
+        self.samp_rate = samp_rate = 1000000
+        self.freq = freq = 915000000
+
+        ##################################################
+        # Blocks
+        ##################################################
+        self.soapy_limesdr_source_0 = None
+        dev = 'driver=lime'
+        stream_args = ''
+        tune_args = ['']
+        settings = ['']
+
+        self.soapy_limesdr_source_0 = soapy.source(dev, "fc32", 1, '',
+                                  stream_args, tune_args, settings)
+        self.soapy_limesdr_source_0.set_sample_rate(0, samp_rate)
+        self.soapy_limesdr_source_0.set_bandwidth(0, 0.0)
+        self.soapy_limesdr_source_0.set_frequency(0, freq)
+        self.soapy_limesdr_source_0.set_frequency_correction(0, 0)
+        self.soapy_limesdr_source_0.set_gain(0, min(max(20.0, -12.0), 61.0))
+        self.soapy_limesdr_sink_0 = None
+        dev = 'driver=lime'
+        stream_args = ''
+        tune_args = ['']
+        settings = ['']
+
+        self.soapy_limesdr_sink_0 = soapy.sink(dev, "fc32", 1, '',
+                                  stream_args, tune_args, settings)
+        self.soapy_limesdr_sink_0.set_sample_rate(0, samp_rate)
+        self.soapy_limesdr_sink_0.set_bandwidth(0, 0.0)
+        self.soapy_limesdr_sink_0.set_frequency(0, freq)
+        self.soapy_limesdr_sink_0.set_frequency_correction(0, 0)
+        self.soapy_limesdr_sink_0.set_gain(0, min(max(20.0, -12.0), 64.0))
+        self.lora_tx_0 = lora_sdr.lora_sdr_lora_tx(
+            bw=125000,
+            cr=1,
+            has_crc=True,
+            impl_head=False,
+            samp_rate=1000000,
+            sf=7,
+         ldro_mode=2,frame_zero_padd=1280 )
+        self.lora_rx_0 = lora_sdr.lora_sdr_lora_rx( bw=125000, cr=1, has_crc=True, impl_head=False, pay_len=255, samp_rate=samp_rate, sf=7, soft_decoding=True, ldro_mode=2, print_rx=[True,True])
+        self.blocks_message_strobe_0 = blocks.message_strobe(pmt.intern("TEST"), 2000)
+        self.blocks_message_debug_0 = blocks.message_debug(True)
+
+
+        ##################################################
+        # Connections
+        ##################################################
+        self.msg_connect((self.blocks_message_strobe_0, 'strobe'), (self.lora_tx_0, 'in'))
+        self.msg_connect((self.lora_rx_0, 'out'), (self.blocks_message_debug_0, 'print'))
+        self.connect((self.lora_tx_0, 0), (self.soapy_limesdr_sink_0, 0))
+        self.connect((self.soapy_limesdr_source_0, 0), (self.lora_rx_0, 0))
+
+
+    def closeEvent(self, event):
+        self.settings = Qt.QSettings("GNU Radio", "LoRaOTALoopback")
+        self.settings.setValue("geometry", self.saveGeometry())
+        self.stop()
+        self.wait()
+
+        event.accept()
+
+    def get_samp_rate(self):
+        return self.samp_rate
+
+    def set_samp_rate(self, samp_rate):
+        self.samp_rate = samp_rate
+        self.soapy_limesdr_sink_0.set_sample_rate(0, self.samp_rate)
+        self.soapy_limesdr_source_0.set_sample_rate(0, self.samp_rate)
+
+    def get_freq(self):
+        return self.freq
+
+    def set_freq(self, freq):
+        self.freq = freq
+        self.soapy_limesdr_sink_0.set_frequency(0, self.freq)
+        self.soapy_limesdr_source_0.set_frequency(0, self.freq)
+
+
+
+
+def main(top_block_cls=LoRaOTALoopback, options=None):
+
+    if StrictVersion("4.5.0") <= StrictVersion(Qt.qVersion()) < StrictVersion("5.0.0"):
+        style = gr.prefs().get_string('qtgui', 'style', 'raster')
+        Qt.QApplication.setGraphicsSystem(style)
+    qapp = Qt.QApplication(sys.argv)
+
+    tb = top_block_cls()
+
+    tb.start()
+
+    tb.show()
+
+    def sig_handler(sig=None, frame=None):
+        tb.stop()
+        tb.wait()
+
+        Qt.QApplication.quit()
+
+    signal.signal(signal.SIGINT, sig_handler)
+    signal.signal(signal.SIGTERM, sig_handler)
+
+    timer = Qt.QTimer()
+    timer.start(500)
+    timer.timeout.connect(lambda: None)
+
+    qapp.exec_()
+
+if __name__ == '__main__':
+    main()
